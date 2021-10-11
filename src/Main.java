1e29fbd71d74cf9c482a33d09e31edab1f0f78a7
Qiyi <qiyi.zhang@mail.utoronto.ca>
2021-10-12 06:29:35 +0800

    zhan7393

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         UserManager um = new UserManager();
-        MainPageController mp = new MainPageController();
+        MainPageController mp = new MainPageController(um);
         mp.run();
 
     }
