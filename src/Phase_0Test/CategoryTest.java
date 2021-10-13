b11df47c11957a067aedb3f703a8e074a93b7191
sanjana <sanjana.girish@mail.utoronto.ca>
2021-10-13 16:00:27 -0400

    Added Test files

--- src/Phase_0Test/CategoryTest.java
+++ src/Phase_0Test/CategoryTest.java
@@ -1,4 +1,23 @@
 package Phase_0Test;
 
+import Phase_0.Category;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
 public class CategoryTest {
+    Category cy;
+
+    @Before
+    public void setUp() {
+        cy = new Category("Category 1");
+    }
+
+    @Test(timeout = 50)
+    public void TestCategory(){
+
+    }
 }
--- src/Phase_0Test/UserLoginControllerTest.java
+++ src/Phase_0Test/UserLoginControllerTest.java
@@ -1,4 +1,11 @@
 package Phase_0Test;
 
+import Phase_0.UserLoginController;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class UserLoginControllerTest {
 }
