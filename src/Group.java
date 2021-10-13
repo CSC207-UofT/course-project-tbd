a7d8f0b46b410fcc5cfd02fed337278244380865
Peter <a2649532831@gmail.com>
2021-10-12 21:39:38 -0400

    Finished implementation of methods

--- src/Group.java
+++ src/Group.java
@@ -1,60 +1,60 @@
-package TBD;
-
-import TBD.Category;
-import TBD.Task;
-import TBD.User;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Group {
-    private User groupLeader;
-    private String groupName;
-    private ArrayList<User> users = new ArrayList<>();
-    private ArrayList<Features> features = new ArrayList<>();
-
-
-    public Group(User groupLead, String groupname) {
-        this.groupLeader = groupLead;
-        this.groupName = groupname;
-    }
-
-    /**
-     * A method that takes the parameter (newCategory) and adds it to the ArrayList
-     */
-    public void addNewFeature(Features target) {
-        this.features.add(target);
-    }
-
-    /**
-     * A method that takes the parameter (target) and removes it from the ArrayList
-     */
-    public void deleteFeature(Features target) {
-        this.features.remove(target);
-    }
-
-    public void addTasktoCategory(Task t, Category c) {
-        c.addTask(t);
-    }
-
-    public void addUsers(List<User> users) {
-        this.users.addAll(users);
-    }
-
-    public ArrayList<User> getUsers() {
-        return this.users;
-    }
-
-    public User getgroupLeader() {
-        return this.groupLeader;
-    }
-
-    public String getgroupName() {
-        return this.groupname;
-    }
-
-    public ArrayList<Features> getFeatures() {
-        return this.features;
-    }
-
-}
+//package TBD;
+//
+//import TBD.Category;
+//import TBD.Task;
+//import TBD.User;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Group {
+//    private User groupLeader;
+//    private String groupName;
+//    private ArrayList<User> users = new ArrayList<>();
+//    private ArrayList<Features> features = new ArrayList<>();
+//
+//
+//    public Group(User groupLead, String groupname) {
+//        this.groupLeader = groupLead;
+//        this.groupName = groupname;
+//    }
+//
+//    /**
+//     * A method that takes the parameter (newCategory) and adds it to the ArrayList
+//     */
+//    public void addNewFeature(Features target) {
+//        this.features.add(target);
+//    }
+//
+//    /**
+//     * A method that takes the parameter (target) and removes it from the ArrayList
+//     */
+//    public void deleteFeature(Features target) {
+//        this.features.remove(target);
+//    }
+//
+//    public void addTasktoCategory(Task t, Category c) {
+//        c.addTask(t);
+//    }
+//
+//    public void addUsers(List<User> users) {
+//        this.users.addAll(users);
+//    }
+//
+//    public ArrayList<User> getUsers() {
+//        return this.users;
+//    }
+//
+//    public User getgroupLeader() {
+//        return this.groupLeader;
+//    }
+//
+//    public String getgroupName() {
+//        return this.groupname;
+//    }
+//
+//    public ArrayList<Features> getFeatures() {
+//        return this.features;
+//    }
+//
+//}
--- src/NormalUser.java
+++ src/NormalUser.java
@@ -4,11 +4,10 @@ public class NormalUser extends User{
 
     public String username;
     public String password;
-    // What is the Group?
 
-    // Two Arraylists of Categories ??? Another one in individualTask
+    //    public ArrayList<GroupTask> myGroups = new ArrayList<GroupTask>();
+
     public ArrayList<Category> myCategories = new ArrayList<Category>();
-
 
     public NormalUser(String username, String password) {
         this.username = username;
@@ -21,4 +20,18 @@ public class NormalUser extends User{
     public String displayUserDetail() {
         return (this.username + " " + this.password);
     }
+
+    /**
+     * A method that takes the parameter (newCategory) and adds it to the ArrayList
+     */
+    public void addNewCategory(Category newCategory) {
+        this.myCategories.add(newCategory);
+    };
+
+    /**
+     * A method that takes the parameter (target) and removes it from the ArrayList
+     */
+    public void deleteCategory(Category target) {
+        this.myCategories.remove(target);
+    };
 }
