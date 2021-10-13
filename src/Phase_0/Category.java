83ce30d61e1fcc78fa3f6489a6b19fdba4975b5d
Kevinkt <kktvaidyan@gmail.com>
2021-10-13 18:03:27 -0400

    Edited the category constructor to initialize an arraylist

--- src/Phase_0/Category.java
+++ src/Phase_0/Category.java
@@ -1,12 +1,15 @@
 package Phase_0;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class Category extends Features{
     private String categoryName;    // This is the name of the category/folder
     private ArrayList<Task> tasks;  // These are all the tasks contained in that category
 
-    public Category (String name) {this.categoryName = name;
+    public Category (String name) {
+        this.categoryName = name;
+        this.tasks = new ArrayList<>();
     }
 
     public void addTask(Task t){
@@ -15,11 +18,11 @@ public class Category extends Features{
     }
 
     public String getCategoryName() {
-        return categoryName;
+        return this.categoryName;
     }
 
     public ArrayList<Task> getTasks() {
-        return tasks;
+        return this.tasks;
     }
 
 
