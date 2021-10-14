500bd53b73840e52a605cb2c419c09f9bf82e8a5
Dinofio <hieu2002>
2021-10-14 15:51:59 -0400

    Modified the GroupManager class #TODO: implement a method in Group.java that removes a given user from a group

--- src/Phase_0/GroupManager.java
+++ src/Phase_0/GroupManager.java
@@ -10,8 +10,9 @@ public class GroupManager extends TaskManager{
     public HashMap<User, ArrayList<Group>> maps;
 
     /**
-     * Construct a TBD.Group, giving them the given category,
-     * users, groups and maps
+     * Construct a TBD.GroupManager, giving them the given maps
+     * attribute with User as keys and ArrayList contains Group as
+     * values
      */
     public GroupManager() {
         this.maps = new HashMap<>();
@@ -38,5 +39,55 @@ public class GroupManager extends TaskManager{
         for (User i: users) {
             this.maps.get(i).remove(group);
         }
+    }
+
+    /**
+     * This method returns a List of User in a given group name
+     * @param group the wanted group
+     * @return List of User in the wanted group
+     */
+    public ArrayList memberList(Group group) {
+        return group.getUsers();
+    }
+
+    /**
+     * This method checks whether the given name
+     * has been taken
+     * @param group the wanted group
+     * @return true if there exists a group with
+     * the same name and false otherwise
+     */
+    public boolean checkGroupName(Group group) {
+        for (User i: this.maps.keySet()) {
+            for (Group j: this.maps.get(i)) {
+                if (group.getgroupName().equals(j.getgroupName())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * This method checks add a given group to the user's
+     * group list
+     * @param group the group that we want to add
+     * @param user given user
+     * @return true if the group is added and false
+     * otherwise
+     */
+    public boolean addGroup(Group group, User user) {
+        return this.maps.get(user).add(group);
+    }
+
+    /**
+     * This method removes a user from the given group
+     * @param group the given group
+     * @param user the member that is removed from group
+     * @return true if successfully remove the member and
+     * false otherwise
+     */
+    public boolean removeMember(Group group, User user) {
+        return this.maps.get(user).remove(group);
     }
 }
