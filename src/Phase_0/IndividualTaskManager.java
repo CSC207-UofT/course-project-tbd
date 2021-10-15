c3b66179bfb94d09171eeb0f0ac703b57578b28c
Fariha Tasnim Oishi <farihaoishee37@gmail.com>
2021-10-15 01:45:43 -0400

    4

--- src/Phase_0/IndividualTaskManager.java
+++ src/Phase_0/IndividualTaskManager.java
@@ -1,4 +1,21 @@
 package Phase_0;
 
-public class IndividualTaskManager {
+public class IndividualTaskManager extends TaskManager{
+
+
+//    public IndividualTaskManager(String information) {
+//        this.newtask = information;
+//    }
+
+    public void createTask(String description){
+        new Task(description);
+    }
+
+    public void completeTask(Task task){
+        task.status = true;
+    }
+
+    public boolean checkIfFinished(Task task){
+        return task.status;
+    }
 }
