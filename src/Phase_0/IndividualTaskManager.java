9c70b939b5fc2bf481d37475ed29cc57524c29a8
sanjana <sanjana.girish@mail.utoronto.ca>
2021-10-15 01:37:47 -0400

    Individual Task Manager and Task done

--- src/Phase_0/IndividualTaskManager.java
+++ src/Phase_0/IndividualTaskManager.java
@@ -1,4 +1,16 @@
 package Phase_0;
 
-public class IndividualTaskManager {
+public class IndividualTaskManager extends TaskManager{
+
+    public void createTask(String description){
+         new Task(description);
+    }
+
+    public void completeTask(Task task){
+        task.status = true;
+    }
+
+    public boolean checkIfFinished(Task task){
+        return task.status;
+    }
 }
