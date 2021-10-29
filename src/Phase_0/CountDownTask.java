61a61eac65d61aa7b0669f61bae9f53b67eb0aca
sanjana <sanjana.girish@mail.utoronto.ca>
2021-10-29 18:17:24 -0400

    CountDownTask introduced + TomatoClock

---
+++ src/Phase_0/CountDownTask.java
@@ -0,0 +1,16 @@
+package Phase_0;
+
+
+import java.time.LocalDate;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CountDownTask extends Task{
+
+
+    public CountDownTask(String title, String information, LocalDate date) {
+        super(title, information, date);
+    }
+
+
+}
