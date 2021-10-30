d5a7e264a1b039caf656563b255aa8189cc275f4
Kevinkt <kktvaidyan@gmail.com>
2021-10-30 16:47:00 -0400

    Partially implemented ViewGroupController which allows the user to view contents in the groups the user has joined. Need to call Aryan's method.

--- src/Phase_0/ViewGroupController.java
+++ src/Phase_0/ViewGroupController.java
@@ -3,6 +3,7 @@ package Phase_0;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class ViewGroupController {
@@ -13,43 +14,46 @@ public class ViewGroupController {
     private UserManager um;
     private GroupManager gm;
     private User currentUser;
-    private final ViewGroupPresenter vgp= new ViewGroupPresenter;
+    // private final ViewGroupPresenter vgp= new ViewGroupPresenter;
 
-    public ViewGroupController(UserManager um, GroupManager gm){
+    public ViewGroupController(UserManager um, GroupManager gm) {
         this.gm = gm;
         this.um = um;
     }
 
-    public void run(){
+    public void run() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while(){
-            
+        ArrayList<Group> groups;
+        StringBuilder s = new StringBuilder();
+        groups = currentUser.getMyGroups();
+        for (int i = 0; i < groups.size(); ++i) {
+            // Creates a string showing all the groups the user has and options to click on them.
+            s.append(i).append(" ").append(groups.get(i).getgroupName()).append("\n");
         }
-        try{
-            String option = "1";
-            while(Objects.equals(option, "1")){
-                jgp.askGroupName();
-                String groupName = reader.readLine();
-                if(!gm.checkGroupExists(groupName)){ // Check whether group name exists if not:-
-                    jgp.groupNameNotInMap();
-                    option = reader.readLine();
+        s.append("Enter x to go back");
+        boolean flag = true;
+        try {
+            while (flag) {
+                System.out.println(s); // Gets input from user
+                String option = reader.readLine();
+                if (Objects.equals(option, "x")) {
+                    // If the user enters x, we terminate and go back to previous page.
+                    flag = false;
                 }
-                else if(gm.checkIfIn(groupName, currentUser)){
-                    // If group name exists and user in group already
-                    jgp.alreadyInGroup(groupName);
-                    option = reader.readLine();
+                else if(Integer.parseInt(option) >= groups.size()){
+                    // If the user enters a number greater than the no of groups user is in, we ask user to
+                    // enter again.
+                    System.out.println("Sorry! The group does not exist. Enter again!");
                 }
                 else{
-                    // Else if group name exists and user is not in the group. Add to group.
-                    gm.addUserToGroup(groupName, currentUser);
-                    // um.addGroupToUser(currentUser, gm.getGroupByName(groupName));
-                    jgp.joinSuccess(groupName);
-                    option = "2";
+                    // We call the controller for the corresponding group.
+                    // TODO: Call Aryans method here
                 }
             }
-            jgp.lines();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Please type a valid number");
-        }
+            }
+
     }
+
 }
