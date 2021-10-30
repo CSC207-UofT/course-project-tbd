9d3894daf548801f3bea5ef289989ae27f24ac6a
oishi37 <fariha.oishi@mail.utoronto.ca>
2021-10-30 01:52:25 -0400

    Added CategoryPagePresenter

---
+++ src/Phase_0/CategoryPagePresenter.java
@@ -0,0 +1,38 @@
+package Phase_0;
+
+public class CategoryPagePresenter {
+
+    public CategoryPagePresenter(){
+    }
+
+
+    public void availableOptions(){
+        System.out.println("--------------------");
+        System.out.print("Your options:\n" +
+                "1, Back\n" +
+                "2. Add Category:\n" +
+                "3. View Category \n" +
+                "Your answer here: ");
+    }
+    public void giveNewCategoryName(){
+        System.out.println("--------------------");
+        System.out.print("Enter Category Title:\n");
+    }
+
+    public void giveCategoryDetail(){
+        System.out.println("--------------------");
+        System.out.println("Enter Category Detail:");
+    }
+
+    public void CategoryAdd(){
+        System.out.println("Your Category has been created and added");
+    }
+
+    public void CategoryNotPresent(){
+        System.out.println("Sorry, the Category is not there in our database");
+    }
+
+    public void displayCategory(){
+        System.out.println("The Category are :");
+    }
+}
