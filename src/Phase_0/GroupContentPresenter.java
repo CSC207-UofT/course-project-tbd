381e53b41b2ee7cc0a96d43d582264781e457e27
aryanbagga <aryanbagga8@gmail.com>
2021-10-31 18:02:56 -0400

    done

---
+++ src/Phase_0/GroupContentPresenter.java
@@ -0,0 +1,16 @@
+package Phase_0;
+
+public class GroupContentPresenter {
+
+    public void instructions() {
+        System.out.println("""
+                Type 1 to go to the homepage\s
+                Type 2 to go to the Group Task Page\s
+                Type 3 to access the Group Chat\s
+                Type 4 to go back\s
+                Type 0 to exit""");
+    }
+    public void askMessage() {
+        System.out.println("Please enter a message");
+    }
+}
