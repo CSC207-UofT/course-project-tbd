db42fc816c86fbb911b4520fa7c7f5976a4ffe09
Kevinkt <kktvaidyan@gmail.com>
2021-10-31 20:21:51 -0400

    Made some changes to the presenter

--- src/Phase_0/GroupPageController.java
+++ src/Phase_0/GroupPageController.java
@@ -1,5 +1,6 @@
 package Phase_0;
 
+import javax.swing.text.View;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -37,6 +38,12 @@ public class GroupPageController {
                     lgc.run();
                     gpp.welcomeLine();
                     input = reader.readLine();
+                }
+                else if (input.equals("4")){
+                    System.out.println("test");
+                    ViewGroupController vgc = new ViewGroupController(um, gm, user);
+                    vgc.run();
+
                 }
                 else {
                     gpp.welcomeLine();
