71b9e1664ea8b09a5fd4c224e86140c25cf9b815
Kevinkt <kktvaidyan@gmail.com>
2021-11-07 20:27:27 -0500

    Added a view folders controller to view all folders and ask user for input to view a specific folder/category

---
+++ src/Phase_1/Controllers_Gateways_Presenters/ViewFoldersPresenter.java
@@ -0,0 +1,33 @@
+package Phase_1.Controllers_Gateways_Presenters;
+
+import Phase_1.Entity.Category;
+import Phase_1.UseCaseClass.GroupManager;
+import Phase_1.UseCaseClass.TaskManager;
+import Phase_1.UseCaseClass.UserManager;
+
+public class ViewFoldersPresenter {
+    private String groupId;
+    private UserManager um;
+    private TaskManager tm;
+    private GroupManager gm;
+    private String userId;
+
+    public ViewFoldersPresenter(UserManager um, TaskManager tm, GroupManager gm, String userId, String groupId){
+        this.groupId = groupId;
+        this.userId = userId;
+        this.um = um;
+        this.gm = gm;
+        this.tm = tm;
+    }
+
+    public void askInput(){
+        StringBuilder s = new StringBuilder();
+        int i = 0;
+        for (Category c : gm.getGroupById(groupId).getCategories()) {
+            s.append(i).append(c.toString()).append("\n");
+            i = i + 1;
+        }
+        s.append("Enter any other key to exit");
+        System.out.println(s);
+    }
+}
