9067160fb848a81f5ebbda8c45f3e50a6e31e691
Qiyi <qiyi.zhang@mail.utoronto.ca>
2021-11-12 00:11:24 +0800

    GroupPageGui

--- src/Phase_1/GUI/GUImain.java
+++ src/Phase_1/GUI/GUImain.java
@@ -2,17 +2,19 @@ package Phase_1.GUI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GUImain extends Application {
+    Stage window;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(GUImain.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 830, 500);
-        primaryStage.setTitle("Hello!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        window = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("GroupPageGUI.fxml"));
+        Scene scene = new Scene(root, 400, 600);
+        window.setScene(scene);
+        window.show();
     }
 
     public static void main(String[] args){
