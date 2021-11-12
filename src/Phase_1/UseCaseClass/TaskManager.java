2f68d2be1e8cb02b3af67f7845fe053dcd24f7f1
owen huang <o.huang@mail.utoronto.ca>
2021-11-12 15:10:14 -0500

    added documentations and fixes

--- src/Phase_1/UseCaseClass/TaskManager.java
+++ src/Phase_1/UseCaseClass/TaskManager.java
@@ -3,24 +3,50 @@ package Phase_1.UseCaseClass;
 import Phase_1.Entity.Category;
 import Phase_1.Entity.Task;
 
+/**
+ * This TaskManager is the use case that is responsible for accessing and manipulating attributes and methods
+ * in Task and its child classes.
+ *
+ * @author  Owen Huang
+ * @author  placeholder
+ */
 public class TaskManager {
 
+    /**
+     * set the status of the task to complete
+     *
+     * @param task is the task to be finished
+     */
     public void completeTask(Task task){
         task.setStatus(true);
     }
 
-    public boolean checkIfFinished(Task task){
-        return task.getStatus();
-    }
-
+    /**
+     * Check whether a string taskName is equal the task's title
+     *
+     * @param task is the task we want to check
+     * @param taskName is a string of a task name that we want to check
+     */
     public boolean checkTaskByName(Task task, String taskName){
         return task.getTaskName().equals(taskName);
     }
 
+    /**
+     * Adds a task to a category
+     *
+     * @param task is a Task object we want to add
+     * @param category is the Category object we want to add task to
+     */
     public void addTaskToCategory(Category category, Task task){
         category.addTask(task);
     }
 
+    /**
+     * Display all the tasks in a category to the screen
+     *
+     * @param category is the category we want to view the tasks of
+     * @return a string representation of all tasks in category
+     */
     public String displayTask(Category category){
         StringBuilder s = new StringBuilder();
         for(Task t: category.getTasks()){
@@ -29,10 +55,24 @@ public class TaskManager {
         return s.toString();
     }
 
+    /**
+     * check whether a task is in the category
+     *
+     * @param t is the task we want to check
+     * @param category is the category we want to check
+     * @return a true of task is in category, false otherwise
+     */
     public boolean checkTask(Category category, Task t) {
         return category.tasks.contains(t);
     }
 
+    /**
+     * Get a specific task in category by the given task name
+     *
+     * @param taskName is the task name of the task we want to search for in category
+     * @param category is the category we want to search task name in
+     * @return the task if it has the same name as taskName, null otherwise
+     */
     public Task getTaskByName(Category category, String taskName) {
         for (Task t: category.tasks){
             if (this.checkTaskByName(t, taskName)){
