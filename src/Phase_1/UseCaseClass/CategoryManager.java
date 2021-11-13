3e3f11481a8a390f2f977869b90012297f5dd786
oishi37 <fariha.oishi@mail.utoronto.ca>
2021-11-13 10:13:32 -0500

    Added documentation to the classes.

--- src/Phase_1/UseCaseClass/CategoryManager.java
+++ src/Phase_1/UseCaseClass/CategoryManager.java
@@ -3,14 +3,31 @@ package Phase_1.UseCaseClass;
 import Phase_1.Entity.*;
 
 public class CategoryManager {
+    /**
+     * This CategoryManager is the use case that is responsible for accessing  attributes and methods
+     * in Category.
+     */
 
     public Category createCategory(String description){
         return new Category(description);
     }
 
+    /**
+     * This method checks the user had this category object.
+     * @param user the object user.
+     * @param c the object category.
+     * @return whether the user has category object.
+     */
+
     public boolean checkCategory(User user, Category c) throws NullPointerException {
         return user.getMyCategories().contains(c);
     }
+    /**
+     * This method checks  with the name of the category with the given user.
+     * @param user the object user
+     * @param categoryName the string representation of the object category.
+     * @return object category if the user has that category.
+     */
 
     public Category getCategoryByName(User user, String categoryName) {
         for (Category c: user.getMyCategories()){
@@ -33,6 +50,13 @@ public class CategoryManager {
     public boolean checkCategoryByName(Category category, String categoryName) {
         return category.getCategoryName().equals(categoryName);
     }
+    /**
+     * This method checks whether the category is in the given group.
+     * @param categoryName the string representation of the object category.
+     * @param groupById the object group.
+     * @return true if the category is in the group.
+     */
+
 
     public Category getCategoryByGroup(String categoryName, Group groupById) {
         for (Category c: groupById.getCategories()){
