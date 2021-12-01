f6988f987f7e3c3ff46bca16f08494bafaaf48a0
oishi37 <fariha.oishi@mail.utoronto.ca>
2021-12-01 14:49:26 -0500

    Trying to fix AddTaskController

--- src/Phase_1/GUI/AddTaskController.java
+++ src/Phase_1/GUI/AddTaskController.java
@@ -1,7 +1,11 @@
 package Phase_1.GUI;
 
 import Phase_1.Entity.Category;
+import Phase_1.Entity.NormalUser;
 import Phase_1.Entity.Task;
+import Phase_1.Entity.TaskWithDueDate;
+import Phase_1.UseCaseClass.CategoryManager;
+import Phase_1.UseCaseClass.NotificationManager;
 import Phase_1.UseCaseClass.TaskManager;
 import Phase_1.UseCaseClass.UserManager;
 import javafx.fxml.FXML;
@@ -22,6 +26,9 @@ public class AddTaskController implements Initializable {
     TaskManager tm;
     UserManager um;
     Category c;
+    String userId;
+    CategoryManager cm;
+    NotificationManager nm;
 
     @FXML
     TextField title;
@@ -40,6 +47,8 @@ public class AddTaskController implements Initializable {
     Hyperlink goback;
 
     public void setTm(TaskManager tm) {this.tm = tm;}
+    public void setUserId(String userId){this.userId = userId;}
+    public void setNm(NotificationManager nm) {this.nm = nm;}
     public void setUm(UserManager um) {
         this.um = um;
     }
@@ -49,7 +58,6 @@ public class AddTaskController implements Initializable {
     public void setC(Category c){this.c = c;}
 
     public void addTask() throws IOException {
-        GUImain guiMain = new GUImain();
         String name = title.getText();
         Success.setText("");
         String info = information.getText();
@@ -64,8 +72,15 @@ public class AddTaskController implements Initializable {
                 int day = Integer.parseInt(formattedDate.get(2));
                 int hour = Integer.parseInt(formattedDate.get(3));
                 int minute = Integer.parseInt(formattedDate.get(4));
-                tm.createTask(name, info, year, month, day, hour, minute);
-                Success.setText("Task Successfully Created");
+                if (name.isEmpty()){
+                    Success.setText("Please Enter valid Task Name");
+                } else {
+                    TaskWithDueDate task = tm.createTask(name, info, year, month, day, hour, minute);
+                    nm.addTaskWithDueDate(task);    // add to notification manager for creating alarm for task
+                    um.addTask(um.getUserById(userId), task,
+                            cm.getCategoryByName(um.getUserById(userId), c.getCategoryName()));
+                    Success.setText("Task Successfully Created");
+                }
             } catch (UnsupportedOperationException e) {     // exception thrown when user schedules a date in the past
                 System.out.println(e.getMessage());
             } catch (IndexOutOfBoundsException e2) {     // when the user's date input does not follow the format
@@ -78,10 +93,13 @@ public class AddTaskController implements Initializable {
             }
         }
         if(ans.equals("No")) {
-            tm.createTask(name, info);
-            Success.setText("Task Successfully Created");
-
-
+            if (name.isEmpty()){
+                Success.setText("Please Enter valid Task Name");
+            } else {
+                um.addTask(um.getUserById(userId), tm.createTask(name, info),
+                        cm.getCategoryByName(um.getUserById(userId), c.getCategoryName()));
+                Success.setText("Task Successfully Created");
+            }
         }
     }
 
